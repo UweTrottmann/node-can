10b09f2d534ae3bb642ab9e545178e5e45613bfc
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-03-18 14:51:25 +0100

    Add partial JSON parser (does not parse nodes for now.)

--- parse_json_config.js
+++ parse_json_config.js
@@ -6,6 +6,7 @@
  */
 function parseJsonConfiguration(fileName) {
   var jsonFile;
+
   try {
     // load the config file
     jsonFile = require(fileName);
@@ -13,16 +14,65 @@ function parseJsonConfiguration(fileName) {
     return new Error("Failed to load config: " + err.message);
   }
 
-  var result = {}; // Result will be a dictionary describing the whole network
+  var network = {};
 
-  result.nodes = {};
+  // Nodes are not supported right now
 
-  result.buses = {};
-  for (b in jsonFile.buses) {
-    
+  // Buses
+  network.buses = {};
+  for (bus in jsonFile.buses) {
+    network.buses[bus] = {};
+    var parsedBus = network.buses[bus];
+
+    parsedBus['messages'] = [];
+    var messages = jsonFile.buses[bus].messages;
+    for (var m = 0; m < messages.length; m++) {
+      var message = messages[m];
+
+      var parsedMessage = {
+        name: message.name,
+        id: parseInt(message.id),
+        ext: message.format == 'extended',
+        triggered: message.triggered == 'true',
+        length: message.length,
+        interval: message.interval ? parseInt(message.interval) : 0,
+        signals: []
+      };
+
+      parsedBus['messages'].push(parsedMessage);
+
+      // Signals for this message
+      var maxOffset = 0;
+      var signals = message.signals;
+      for (var s = 0; s < signals.length; s++) {
+        var signal = signals[s];
+        
+        var parsedSignal = {
+          name: signal.name,
+          bitLength: signal.bitLength ? parseInt(signal.bitLength) : 1,
+          bitOffset: signal.bitOffset ? signal.bitOffset : 0,
+          endianess: signal.endianess ? signal.endianess : 'little',
+        };
+
+        // get max offset
+        var signalOffset = parsedSignal.bitOffset + parsedSignal.bitLength;
+        if (signalOffset > maxOffset) {
+          maxOffset = signalOffset;
+        }
+
+        parsedMessage.signals.push(parsedSignal);
+      }
+
+      // calc a length if none is given
+      if (!parsedMessage.length) {
+        parsedMessage.length = parseInt(maxOffset / 8);
+        if (maxOffset % 8 > 0)
+          parsedMessage.length++;
+      }
+    }
   }
 
-  return result;
+  return network;
 }
 
 exports.parseJsonConfiguration = parseJsonConfiguration;
